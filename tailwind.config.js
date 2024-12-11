e63752fdb06c609316d68ccdc3a33d40738c75be
anis <156479233+anis31620007@users.noreply.github.com>
2024-12-11 18:53:32 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // Adjust based on your project structure
+    "./node_modules/@fortawesome/fontawesome-free/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
