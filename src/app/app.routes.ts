bc84efd8a3b2faa884f9d477e14768205d1761d6
Houda <mn_mesbah@esi.dz>
2024-12-21 16:41:24 +0100

    Ajout de la page créer Dpi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { DossierpatientsComponent } from './pages/dossierpatients/dossierpatients.component';
+import { CreerDpiComponent } from './pages/creer-dpi/creer-dpi.component';
+
 
 export const routes: Routes = [
-  { path: '', component: LandingPageComponent},
+  { path: '', component: DossierpatientsComponent},
   {path: 'patient', component: DossierpatientsComponent},
 {path: 'landing', component:  LandingPageComponent},
+{path: 'creerdpi', component:  CreerDpiComponent},
 ];
 
 @NgModule({
---
+++ src/app/pages/creer-dpi/creer-dpi.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-creer-dpi',
+  imports: [],
+  templateUrl: './creer-dpi.component.html',
+  styleUrl: './creer-dpi.component.css'
+})
+export class CreerDpiComponent {
+
+}
